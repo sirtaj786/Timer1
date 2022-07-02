9b9b3ebe3ff491e070a2d6d36ba6b2a72759f8cc
Srk78699 <ksirtaj29@gmail.com>
2022-07-02 12:44:15 +0530

    With count Dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Timer from "./Components/Timer"
+import TimercountSettimeout from './Components/TimerCountSettimeout';
 
 function App() {
   return (
     <div className="App">
       <Timer/>
+      <TimercountSettimeout/>
       
     </div>
   );
--- src/Components/TimerCountSettimeout.jsx
+++ src/Components/TimerCountSettimeout.jsx
@@ -1,17 +1,18 @@
+
 import { useEffect, useState } from "react";
 
-export default function Timer(){
+export default function TimercountSettimeout(){
   const[count,setCount]=useState(0)
   useEffect(()=>{
     setTimeout(()=>{
       setCount(count+1)
-      console.log(`count${count}`)
     },1000)
-  },[])
+  },[count])
   return(
     <div>
+        <h1>Dependency with count</h1>
       <h1>Counter:{count}</h1>
       <h3>Update every second</h3>
       </div>
   )
-}  
+}
