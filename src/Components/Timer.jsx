250a48c9d273370366a677223fd6dc67f75ce84d
Srk78699 <ksirtaj29@gmail.com>
2022-07-02 12:08:52 +0530

    timer

---
+++ src/Components/Timer.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function TimercountSettimeout(){
+  const[count,setCount]=useState(0)
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCount(count+1)
+    },1000)
+  },[count])
+  return(
+    <div>
+      <h1>Counter:{count}</h1>
+      <h3>Update every second</h3>
+      </div>
+  )
+}
